ad71d6b68c64d2cbaa758ab7683df31fb828952d
JPMartha <sathachie@gmail.com>
2015-12-27 22:48:48 +0900

    Initial Commit

---
+++ SharingOnInstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharingOnInstagram
+//
+//  Created by sathachie on 2015/12/27.
+//  Copyright © 2015年 JPMartha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
